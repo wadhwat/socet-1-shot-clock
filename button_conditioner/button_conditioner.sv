353527b46d0d4128e93d0601d9e0158a40226d56
Jean-Milan Albarede <jeanmilan.albarede@gmail.com>
2026-03-28 14:53:44 -0400

    Added 2 folders, 1 for tick_gnenerator module and 1 for button_conditioner module

---
+++ button_conditioner/button_conditioner.sv
@@ -0,0 +1,11 @@
+module button_conditioner #(
+    parameter integer DEBOUNCE_CYCLES = 100_000 // Number of clock cycles for debouncing
+)(
+    intput wire clk, 
+    intput logic nrst,
+    intput logic raw_button_signal
+);
+
+
+
+endmodule
