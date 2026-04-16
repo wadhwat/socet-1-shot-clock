fdd15d2a40f5dfc415c282c1721de619631972c9
twadhwa <95930095+wadhwat@users.noreply.github.com>
2026-04-16 18:07:33 -0400

    added pseudocode to main_driver

--- sv/src/main_driver.sv
+++ sv/src/main_driver.sv
@@ -11,5 +11,41 @@ module main_driver (
     score_driver s1 ();
     shotclock_driver sh1 ();
 
+    "LED 1 [7:0] (a-g & DP)"
+"LED_2 [7:0] (a-g & DP)"
+...
+"LED 12 [7:0] (a-g & DP)"
+
+colon 1
+colon 2
+colon 3
+
+buzzer signal
+
+6 LED inputs
+
+input clk
+input 480hz
+
+num = 1;
+while(true):
+  num++;
+  switch(num)
+  {
+  case 1:
+    main_segments_pin_out[0] <= LED_1[0]
+	...
+    M_S_P_O[11] <= LED_1[11]
+  case 2:
+    ...
+  case 12:
+    M_S_P_O[0] <= LED_12[0]
+	...
+    M_S_P_O[11] <= LED_12[11]
+  }
+
+  if(num == 12)
+    num = 1;
+  
 
 endmodule
