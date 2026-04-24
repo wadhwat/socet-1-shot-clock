abdbd9a4258f2797171ff94084850ed0c3122bb4
Jean-Milan Albarede <jeanmilan.albarede@gmail.com>
2026-04-23 21:14:32 -0400

    IMPORTANT: Changed logic of score tracker so that possessuion state input means away team when 1 and home team when 0

--- sv/src/score_tracker.sv
+++ sv/src/score_tracker.sv
@@ -3,7 +3,7 @@ module score_tracker(
     input logic nrst,
     input logic plus_one_possession_pulse,
     input logic minus_one_possession_pulse,
-    input logic possession_state, // 1 is the home team and 0 is the away team
+    input logic possession_state, // 1 is the away team and 0 is the home team
     output logic [7:0] home_score,
     output logic [7:0] away_score
 );
@@ -15,18 +15,18 @@ module score_tracker(
         end
         else if (plus_one_possession_pulse) begin
             if (possession_state)
-                home_score <= home_score + 8'd1;
-            else
                 away_score <= away_score + 8'd1;
+            else
+                home_score <= home_score + 8'd1;
         end
         else if (minus_one_possession_pulse) begin
             if (possession_state) begin
-                if (home_score != 8'd0)
-                    home_score <= home_score - 8'd1;
-            end
-            else begin
                 if (away_score != 8'd0)
                     away_score <= away_score - 8'd1;
+            end
+            else begin
+                if (home_score != 8'd0)
+                    home_score <= home_score - 8'd1;
             end
         end
     end
--- sv/src/top.sv
+++ sv/src/top.sv
@@ -21,18 +21,21 @@ module top (
     logic tick_10Hz, tick_1kHz, tick_2640Hz;
     logic btn_start_stop, btn_possession, btn_score_up, btn_score_down, btn_shot_reset;
     logic [1:0] pos_led_wire;
+    logic [3:0] period_led_wire;
+    logic [7:0] scr_ss1, scr_ss2, scr_ss3, scr_ss4; 
+    logic posession_state_wire;
 
     main_driver m1 (
         .clk(clk),
         .tick_2640Hz(tick_2640Hz),
         .n_rst(n_rst),
-        .quad_led(),
+        .period_led(period_led_wire),
         .pos_led(pos_led_wire),
         .gc_ss1(gc_ss1), .gc_ss2(gc_ss2), .gc_ss3(gc_ss3), .gc_ss4(gc_ss4),
         .scr_ss1(scr_ss1), .scr_ss2(scr_ss2), .scr_ss3(scr_ss3), .scr_ss4(scr_ss4),
-        .scr_colon(scr_colon),
+        .scr_colon(1'b0), 
         .sc_ss1(sc_ss1), .sc_ss2(sc_ss2), .sc_ss3(sc_ss3), .sc_ss4(sc_ss4),
-        .sc_colon(),
+        .sc_colon(1'b0),
         .buzzer_in(),
 
         .main_segments_pin_out(display_segments),
@@ -40,16 +43,43 @@ module top (
         .gc_colon(),
         .sc_colon_out(), 
         .scr_colon_out(), 
-        .quad_led_out(), 
+        .period_led_out(period_leds), 
         .pos_led_out(possession_leds)
+    );
+
+    score_driver sd1 (
+        .home_score(), 
+        .away_score(), 
+        .scr_ss1(scr_ss1),
+        .scr_ss2(scr_ss2),
+        .scr_ss3(scr_ss3),
+        .scr_ss4(scr_ss4)
+    );
+
+    score_tracker st1 (
+        .clk(clk),
+        .nrst(n_rst),
+        .plus_one_possession_pulse(btn_score_up),
+        .minus_one_possession_pulse(btn_score_down),
+        .possession_state(posession_state_wire), // Assuming home 0
+        .home_score(), // Connect to score_driver
+        .away_score()  // Connect to score_driver 
     );
 
     possession_ctrl pc1 (  //Is a driver as well already technically so...
         .clk(clk),
         .n_rst(n_rst),
         .possession_toggle_pulse(btn_possession),
-        .possession_state(), //COMPLETE THIS OUTPUT
+        .possession_state(posession_state_wire), //COMPLETE THIS OUTPUT
         .possession_leds(pos_led_wire)
+    );
+
+    period_ctrl pc2 (
+        .clk(clk),
+        .n_rst(n_rst),
+        .increment(), //from control fsm i think
+        .period_state(), //complete output to needed module
+        .period_leds(period_led_wire)
     );
 
     tick_generator #(
